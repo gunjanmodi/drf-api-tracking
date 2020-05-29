5cc175d34bd81d3f8d615016c8bb41fd911cc1ab
Gunjan Modi <er.gunjanmodi@gmail.com>
2020-05-29 16:17:26 +0530

    Adding generic relation in the API logs

--- rest_framework_tracking/models.py
+++ rest_framework_tracking/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 from .base_models import BaseAPIRequestLog
 
 
 class APIRequestLog(BaseAPIRequestLog):
     verb = models.CharField(max_length=500, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
